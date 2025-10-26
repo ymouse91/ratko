6a60ea7e77edac9c5e871d70789728c3794a2e43
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-10-26 13:59:52 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open('ristisanaratkoja-v4').then(c=>c.addAll([
-    './','index.html','manifest.json','suodatetut.txt','icon-192.png','icon-512.png'
+    './','./index.html','./manifest.json','./suodatetut.txt','./icon-192.png','./icon-512.png'
   ])));
 });
 
